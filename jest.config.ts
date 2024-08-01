a776131b1f244244b5bd918c4f9acff02c985fa0
Higor Jardini <contac@higorjardini.dev>
2024-08-01 07:35:07 -0300

    7.0.0

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
+  testMatch: ["**/__tests__/**/*.ts?(x)", "**/?(*.)+(spec|test).ts?(x)"],
+  moduleFileExtensions: ["ts", "js", "json", "node"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
 };
 
 export default config;
